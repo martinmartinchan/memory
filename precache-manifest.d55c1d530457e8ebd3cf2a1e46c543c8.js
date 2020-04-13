c4dc312391770ad69d542bd7e5525abda1fa73a7
martin chan <martinchan994@gmail.com>
2020-04-13 18:22:01 +0200

    Updates

---
+++ precache-manifest.d55c1d530457e8ebd3cf2a1e46c543c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07871a8f23ab43554b14f9ac0a337ba2",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "7e422035178eb8c87c34",
+    "url": "/memory/static/css/main.2c4c50b9.chunk.css"
+  },
+  {
+    "revision": "de2be4d6cc50889a7f71",
+    "url": "/memory/static/js/2.f96afff4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory/static/js/2.f96afff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e422035178eb8c87c34",
+    "url": "/memory/static/js/main.eb6ebd70.chunk.js"
+  },
+  {
+    "revision": "70f7300e0526a5bfd17e",
+    "url": "/memory/static/js/runtime-main.4d1b8c16.js"
+  }
+]);
